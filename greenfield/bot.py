a50da1140aece60f5aa6c5f190bb147010c388dd
Matty Taylor <70635651+tayl0rm@users.noreply.github.com>
2023-09-25 16:57:40 +0000

    Format .py

--- greenfield/bot.py
+++ greenfield/bot.py
@@ -21,6 +21,7 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.command(name="valheim-up")
 async def valheim_up(ctx):
     await ctx.channel.send('The Valheim Server is spinning up!')
@@ -33,11 +34,13 @@ async def valheim_up(ctx):
     request = service.instances().start(project=project, zone=zone, instance=instance)
     response = request.execute()
 
-    response = service.instances().get(project=project, zone=zone, instance=instance).execute()
+    response = service.instances().get(
+        project=project, zone=zone, instance=instance).execute()
     vahleim_server_ip = response['networkInterfaces'][0]['accessConfigs'][0]['natIP']
 
     await asyncio.sleep(60)
     await ctx.channel.send(f'The Valheim Server is accessible at {vahleim_server_ip}!')
+
 
 @bot.command(name="valheim-down")
 async def valheim_down(ctx):
@@ -54,4 +57,4 @@ async def valheim_down(ctx):
     await asyncio.sleep(60)
     await ctx.channel.send('The Valheim Server has shutdown, you can use *!vaheim-up* to restart the server!')
 
-bot.run(os.getenv('DISCORD_TOKEN'))#bot 
+bot.run(os.getenv('DISCORD_TOKEN'))  # bot
