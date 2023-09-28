96acbae2d0cbee1d0f6e847f26172663b9eeafa4
Matty Taylor <70635651+tayl0rm@users.noreply.github.com>
2023-09-28 19:46:34 +0000

    Update backup.tf

--- terraform/game-servers/valheim/backup.tf
+++ terraform/game-servers/valheim/backup.tf
@@ -2,6 +2,5 @@ terraform {
   backend "gcs" {
     bucket      = "terraform_state_backup"
     prefix      = "terraform/state/game-servers/valheim"
-    credentials = "gcloud-service-account.json"
   }
 }
