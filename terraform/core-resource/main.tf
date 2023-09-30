a458eec1282ee879194fac67783eb0d550d8a921
Matty Taylor <70635651+tayl0rm@users.noreply.github.com>
2023-09-30 19:45:19 +0000

    Try to trigger TF workflow

--- terraform/core-resource/main.tf
+++ terraform/core-resource/main.tf
@@ -25,9 +25,3 @@ module "core_network" {
   subnetwork_cidr         = "10.1.0.0/26"
   auto_create_subnetworks = false
 }
-
-//module "backup_bucket" {
-//  source      = "github.com/tayl0rm/terraform-modules/gcs-bucket"
-//  gcp_project = var.gcp_project
-//  name        = "tayl0rm-server-backup"
-// }
